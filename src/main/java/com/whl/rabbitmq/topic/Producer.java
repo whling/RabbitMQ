31ebe117edeb4093f4020772d32143b6dbbd8253
whling <1769778747@qq.com>
2017-07-05 15:00:18 +0800

    topic实现负载均衡

--- src/main/java/com/whl/rabbitmq/topic/Producer.java
+++ src/main/java/com/whl/rabbitmq/topic/Producer.java
@@ -3,9 +3,11 @@ package com.whl.rabbitmq.topic;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.whl.rabbitmq.util.ConnectionUtil;
+
 /**
  * 
  * description: 消息路由测试topic(topic支持routing key通配符模式(#:匹配多个，*:匹配一个))
+ * 
  * @author whling
  * @date 2017年7月4日 下午7:22:00
  *
@@ -14,19 +16,21 @@ public class Producer {
 	private final static String EXCHANGE_NAME = "test_exchange_topic";
 
 	public static void main(String[] argv) throws Exception {
-        // 获取到连接以及mq通道
-        Connection connection = ConnectionUtil.getConnection();
-        Channel channel = connection.createChannel();
+		// 获取到连接以及mq通道
+		Connection connection = ConnectionUtil.getConnection();
+		Channel channel = connection.createChannel();
 
-        // 声明exchange
-        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+		// 声明exchange
+		channel.exchangeDeclare(EXCHANGE_NAME, "topic");
 
-        // 消息内容
-        String message = "id=1001";
-        channel.basicPublish(EXCHANGE_NAME, "item.hello", null, message.getBytes());
-        System.out.println(" [x] Sent '" + message + "'");
+		for (int i = 0; i < 10000; i++) {
+			// 消息内容
+			String message = "id=1001"+"xxxxxxxxxx"+i;
+			channel.basicPublish(EXCHANGE_NAME, "item.hello", null, message.getBytes());
+			System.out.println(" [x] Sent '" + message + "'");
+		}
 
-        channel.close();
-        connection.close();
-    }
+		channel.close();
+		connection.close();
+	}
 }
